f38f0e32c88f3607712a5b6010fce5c9878d7160
Dharunkumar0510 <dharunkumarapd@gmail.com>
2026-03-24 19:09:23 +0530

    Update create_bill.php

--- create_bill.php
+++ create_bill.php
@@ -1,11 +1,1 @@
-<?php
-include 'db.php';
 
-$data = json_decode(file_get_contents("php://input"), true);
-
-$total = $data['total'];
-
-$conn->query("INSERT INTO bills (total) VALUES ($total)");
-
-echo "success";
-?>
